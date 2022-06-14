c13e0246a61dcd2746822da2f964b69636da3e26
Mirtris <mirtris0522@gmail.com>
2022-06-15 07:45:43 +0800

    sleep

--- task.php
+++ task.php
@@ -22,32 +22,89 @@ if ($conn->connect_error) {
 
 
 define("PAGE_LIMIT", 10);
-//抓各頁資料
-$page = (isset($_GET['page']))? intval($_GET['page']): 1;
 
 
-// 分頁數
-
-$sql_count = "SELECT count(*) AS totalRows FROM `project`.`task`";
-$sth_count = $conn->query($sql_count);
-$result_count = $sth_count->fetch_assoc();
-$totalRows = $result_count['totalRows'];
-$totalPages = ceil($totalRows / PAGE_LIMIT);
-
-
-$previousPage = (($page - 1) < 1)? 1 : ($page - 1);
-$nextPage = (($page + 1) > $totalPages)? $totalPages: ($page + 1);
-
-// 一頁幾筆
-$sql =  sprintf(
-	"select * from `project`.`task` LIMIT %s, %s",
-	($page - 1) * PAGE_LIMIT,
-	PAGE_LIMIT
-);
-$result = $conn->query($sql);
+// 將參數存為陣列，好處理
+$urlParams = [];
+parse_str($_SERVER['QUERY_STRING'], $urlParams);
 
 
 
+
+
+
+//處理搜尋
+$sqlMethod = '';
+
+
+
+// 讀取所有資料
+if (!empty($urlParams['searchText'])) {
+	switch ($urlParams['searchType']) {
+		case 'task_ID':
+			$sqlMethod = '=';
+			break;
+		case 'task_type':
+			$sqlMethod = 'like';
+			$urlParams['searchText'] = "'%" . $urlParams['searchText'] . "%'";
+			break;
+		case 'location':
+			$sqlMethod = 'like';
+			$urlParams['searchText'] = "'%" . $urlParams['searchText'] . "%'";
+			break;
+		case 'user_id':
+			$sqlMethod = '=';
+			break;
+	}
+		// 分頁數
+		$sql_count = sprintf(
+			"SELECT count(*) AS totalRows from `project`.`task` where %s %s %s",
+			$urlParams['searchType'],
+			$sqlMethod,
+			$urlParams['searchText']
+		);
+		$sth_count = $conn->query($sql_count);
+		$result_count = $sth_count->fetch_assoc();
+		$totalRows = $result_count['totalRows'];
+		$totalPages = ceil($totalRows / PAGE_LIMIT);
+	
+		//抓各頁資料
+		$page = (isset($_GET['page'])) ? intval($_GET['page']) : 1;
+		$previousPage = (($page - 1) < 1) ? 1 : ($page - 1);
+		$nextPage = (($page + 1) > $totalPages) ? $totalPages : ($page + 1);
+
+	$sql =  sprintf(
+		"select * from `project`.`task` where %s %s %s LIMIT %s, %s",
+		$urlParams['searchType'],
+		$sqlMethod,
+		$urlParams['searchText'],
+		($page - 1) * PAGE_LIMIT,
+		PAGE_LIMIT
+	);
+	echo ("$sql");
+	$result = $conn->query($sql);
+} else {
+
+	// 分頁數
+	$sql_count = "SELECT count(*) AS totalRows FROM `project`.`task`";
+	$sth_count = $conn->query($sql_count);
+	$result_count = $sth_count->fetch_assoc();
+	$totalRows = $result_count['totalRows'];
+	$totalPages = ceil($totalRows / PAGE_LIMIT);
+
+	//抓各頁資料
+	$page = (isset($_GET['page'])) ? intval($_GET['page']) : 1;
+	$previousPage = (($page - 1) < 1) ? 1 : ($page - 1);
+	$nextPage = (($page + 1) > $totalPages) ? $totalPages : ($page + 1);
+
+	// 讀取所有資料
+	$sql =  sprintf(
+		"select * from `project`.`task` LIMIT %s, %s",
+		($page - 1) * PAGE_LIMIT,
+		PAGE_LIMIT
+	);
+	$result = $conn->query($sql);
+}
 
 
 // 狀態
@@ -99,88 +156,93 @@ echo "</ul>";*/
 		</nav>
 	</div>
 
-	<div class="searchGroup">
+	<form action="task.php" method="get">
+		<div class="searchGroup">
 
-		<div class="searchBar">
-			<select class="searchSelection" name="searchType">
-				<option value="task_ID">案件ID</option>
-				<option value="task_type">案件類別</option>
-				<option value="task_location">地點</option>
-				<option value="user_ID">通報人ID</option>
-			</select>
-			<div class="container-1">
-				<span class="submit" name="submit"><i class="fa fa-search"></i></span>
-				<input type="search" class="searchText" name="searchText" placeholder="搜尋...">
+			<div class="searchBar">
+				<select class="searchSelection" name="searchType">
+					<option value="task_ID">案件ID</option>
+					<option value="task_type">案件類別</option>
+					<option value="location">地點</option>
+					<option value="user_ID">通報人ID</option>
+				</select>
+				<div class="container-1">
+					<span class="submit" name="submit" type="submit" onClick="submitForm()"><i class="fa fa-search"></i></span>
+					<input type="search" class="searchText" name="searchText" placeholder="搜尋...">
+				</div>
 			</div>
-		</div>
-
-		<table width="100%" class="board">
-			<thead>
+	</form>
+	<table width="100%" class="board">
+		<thead>
+			<tr align="center">
+				<td class="boardTitle">案件ID</td>
+				<td class="boardTitle">案件類別</td>
+				<td class="boardTitle">案件通報時間</td>
+				<td class="boardTitle">地點</td>
+				<td class="boardTitle">狀態</td>
+				<td class="boardTitle">通報人ID</td>
+			</tr>
+		</thead>
+		<tbody>
+			<?php while ($board = $result->fetch_assoc()) : ?>
+				<?php
+				if ($board['task_type'] == 1) {
+					$board['task_type'] = '廢棄物';
+				} else if ($board['task_type'] == 2) {
+					$board['task_type'] = '落葉';
+				} else if ($board['task_type'] == 3) {
+					$board['task_type'] = '髒污清潔';
+				} else if ($board['task_type'] == 4) {
+					$board['task_type'] = '器物損壞';
+				} else if ($board['task_type'] == 5) {
+					$board['task_type'] = '其他';
+				} ?>
+				<?php
+				if ($board['flag'] == 0) {
+					$board['flag'] = '未處理';
+				} else if ($board['flag'] == 1) {
+					$board['flag'] = '處理中';
+				} else if ($board['flag'] == 2) {
+					$board['flag'] = '已處理';
+				} ?>
 				<tr align="center">
-					<td class="boardTitle">案件ID</td>
-					<td class="boardTitle">案件類別</td>
-					<td class="boardTitle">案件通報時間</td>
-					<td class="boardTitle">地點</td>
-					<td class="boardTitle">狀態</td>
-					<td class="boardTitle">通報人ID</td>
+					<td class="boardInput" name="task_ID"><?php echo $board['task_id'] ?></td>
+					<td class="boardInput" name="task_type"><?php print $board['task_type'] ?></td>
+					<td class="boardInput" name="task_time"><?php print $board['time'] ?></td>
+					<td class="boardInput" name="locatio"><?php print $board['location'] ?></td>
+					<td class="status" name="task_status" bgcolor="<?php if ($board['flag'] === '未處理') {
+																		echo "#A7414A";
+																	} elseif ($board['flag'] === '處理中') {
+																		echo "#F28A30";
+																	} elseif ($board['flag'] === '已處理') {
+																		echo "#ADFF2F";
+																	} ?>"><?php print $board['flag'] ?></td>
+					<td class="boardInput" name="user_ID"><?php print $board['user_id'] ?></td>
 				</tr>
-			</thead>
-			<tbody>
-				<?php while ($board = $result->fetch_assoc()) : ?>
-					<?php
-					if ($board['task_type'] == 1) {
-						$board['task_type'] = '廢棄物';
-					} else if ($board['task_type'] == 2) {
-						$board['task_type'] = '落葉';
-					} else if ($board['task_type'] == 3) {
-						$board['task_type'] = '髒污清潔';
-					} else if ($board['task_type'] == 4) {
-						$board['task_type'] = '器物損壞';
-					} else if ($board['task_type'] == 5) {
-						$board['task_type'] = '其他';
-					} ?>
-					<?php
-					if ($board['flag'] == 0) {
-						$board['flag'] = '未處理';
-					} else if ($board['flag'] == 1) {
-						$board['flag'] = '處理中';
-					} else if ($board['flag'] == 2) {
-						$board['flag'] = '已處理';
-					} ?>
-					<tr align="center">
-						<td class="boardInput" name="task_ID"><?php echo $board['task_id'] ?></td>
-						<td class="boardInput" name="task_type"><?php print $board['task_type'] ?></td>
-						<td class="boardInput" name="task_time"><?php print $board['time'] ?></td>
-						<td class="boardInput" name="task_location"><?php print $board['location'] ?></td>
-						<td class="status" name="task_status" bgcolor="<?php if ($board['flag'] === '未處理') {
-																			echo "#A7414A";
-																		} elseif ($board['flag'] === '處理中') {
-																			echo "#F28A30";
-																		} elseif ($board['flag'] === '已處理') {
-																			echo "#ADFF2F";
-																		} ?>"><?php print $board['flag'] ?></td>
-						<td class="boardInput" name="user_ID"><?php print $board['user_id'] ?></td>
-					</tr>
-				<?php endwhile ?>
+			<?php endwhile ?>
 
 
 
-			</tbody>
-		</table>
-		<nav>
-			<ul class="pagination">
-				<li class="page-item"><a class="page-link" href="task.php?page=<?php print $previousPage ?>">前一頁</a></li>
-				<?php for ($i = 1; $i <= $totalPages; $i++) : ?>
-					<li class="page-item<?php if($page == $i) print'active'?>"><a class="page-link" href="task.php?page=<?php print $i ?>"><?php print $i ?></a></li>
-				<?php endfor ?>
-				<li class="page-item"><a class="page-link" href="task.php?page=<?php print $nextPage ?>">下一頁</a></li>
-			</ul>
-		</nav>
+		</tbody>
+	</table>
+	<nav>
+		<ul class="pagination">
+			<li class="page-item"><a class="page-link" href="task.php?page=<?php print $previousPage ?>">前一頁</a></li>
+			<?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+				<li class="page-item<?php if ($page == $i) print 'active' ?>"><a class="page-link" href="task.php?page=<?php print $i ?>"><?php print $i ?></a></li>
+			<?php endfor ?>
+			<li class="page-item"><a class="page-link" href="task.php?page=<?php print $nextPage ?>">下一頁</a></li>
+		</ul>
+	</nav>
 
 	</div>
 	</div>
 
-
+	<script>
+		function submitForm() {
+			document.forms[0].submit();
+		}
+	</script>
 
 </body>
 
